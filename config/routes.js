34f6311a756123e7e1fbf22e5a0cce2336183c91
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-05-22 06:23:59 +0800

    Added Forms - General Elements page

--- config/routes.js
+++ config/routes.js
@@ -70,6 +70,9 @@ module.exports.routes = {
   },
   'GET /ui/modal': {
     view: 'ui-elements/modal'
+  },
+  'GET /forms/general': {
+    view: 'forms/general'
   }
   /***************************************************************************
   *                                                                          *
